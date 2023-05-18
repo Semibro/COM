d12f23905b3464d7f3b3a0c23dbcb550813499e4
김준형 <wnsgud6232@naver.com>
2023-05-18 13:34:30 +0900

    Feat: Add admin - movies accounts

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from django.contrib.auth import get_user_model
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import *
+
 
 # Create your views here.
+User = get_user_model()
+
+
+@api_view
