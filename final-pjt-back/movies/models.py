5b5d18205fdced053c8f8c56733faaf21362e486
김준형 <wnsgud6232@naver.com>
2023-05-18 14:27:08 +0900

    Feat: Add reviews/models.py

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.conf import settings
 import datetime
 
@@ -23,19 +22,3 @@ class Credits(models.Model):
     actor_profile_path_1 = models.TextField(null=True)
     actor_name_2 = models.CharField(max_length=100, null=True)
     actor_profile_path_2 = models.TextField(null=True)
-
-
-class Reviews(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
-    movie = models.ForeignKey(Movies, on_delete=models.CASCADE, related_name='reviews')
-    rate = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-
-class Comments(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
-    review = models.ForeignKey(Reviews, on_delete=models.CASCADE, related_name='comments')
-    comments = models.CharField(max_length=500)
-    created_at = models.DateTimeField(auto_now_add=True)
