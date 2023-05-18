fa8e5219c9af9e5f278580b6df5dc17cbc1e4676
김준형 <wnsgud6232@naver.com>
2023-05-18 12:06:51 +0900

    Feat: Add serializer - accounts

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from movies.models import Movies, Reviews
+
+
+User = get_user_model()
+
+
+# 사용자
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+# 사용자가 작성한 영화 리뷰
+class ReviewMovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movies
+        fields = ('pk', 'title', 'poster_path', 'vote_average')
