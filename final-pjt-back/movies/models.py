d5faa9ff307b693b25c407c6923e5c1b529e3a94
김준형 <wnsgud6232@naver.com>
2023-05-19 16:58:51 +0900

    Feat: Add django models

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
+import datetime
 
 # Create your models here.
+class Movie(models.Model):
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    poster_path = models.TextField(null=True)
+    release_date = models.DateField(null=True, default=datetime.date.today)
+    vote_average = models.FloatField(null=True)
+    vote_count = models.IntegerField(null=True)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rate = models.IntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
