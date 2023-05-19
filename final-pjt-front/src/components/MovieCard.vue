f91c45b27a31366a2e90127a26cd00b11a7b887e
김준형 <wnsgud6232@naver.com>
2023-05-19 14:23:13 +0900

    Feat: Add views/LikeMovieChooseView.vue

---
+++ final-pjt-front/src/components/MovieCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <div class="movieCard">
+        <div class="movieimgbox">
+          <img :src="imgurl" class="movieimg">
+        </div>
+        <div class="text">
+          <div class="title">
+            {{ movie.title }}
+          </div>
+          <div v-if="movie.overview">
+            {{ movie.overview }}
+          </div>
+          <div v-else>
+            <p> 등록된 줄거리가 없습니다🙏 </p>
+          </div>
+          <div>
+            평점: {{ movie_rate }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'MovieCard',
+  props: {
+    movie: Object
+  },
+  computed: {
+    imgurl() {
+      return `https://image.tmdb.org/t/p/w300/${this.movie.poster_path}`
+    },
+    movie_rate() {
+      return Math.round(this.movie.vote_average / 2)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
