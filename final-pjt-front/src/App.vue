e7eb06ab26adcaef1070900595a35f42c5252831
김준형 <wnsgud6232@naver.com>
2023-05-19 09:26:35 +0900

    Feat: Update vue axios - App HomeView MoviesView ProfileView

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -25,7 +25,19 @@ export default {
       console.log(1)
       this.$router.push({ name: 'home' }).catch(()=>{})
     }
-  }  
+  },
+  created() {
+    axios({
+      method: 'get',
+      url: `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?language=ko-KR&page=1&api_key=b87676597510090177f5217ea5f4d280`
+    })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
 </script>
 
