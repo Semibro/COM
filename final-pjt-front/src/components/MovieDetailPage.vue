958c94f5e2a291534f646d684c5e2b577bd27956
김준형 <wnsgud6232@naver.com>
2023-05-23 16:29:28 +0900

    Feat: Add movie detail - youtube teaser

--- final-pjt-front/src/components/MovieDetailPage.vue
+++ final-pjt-front/src/components/MovieDetailPage.vue
@@ -16,19 +16,27 @@
             {{ detail_movie.overview }}
           </div>
         </div>
+        <iframe width="1000px" height="700px" :src="youtubesrc"></iframe>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 // const API_URL = 'http://127.0.0.1:8000'
+const API_KEY = 'cab515bc24cb97ee07d658f5fd0aa1a7'
 
 export default {
   name: 'MovieDetailPage',
   props: {
     detail_movie: Object,
+  },
+  data() {
+    return {
+      youtubeId: null,
+      youtubesrc: null,
+    }
   },
   computed: {
     imgurl() {
@@ -39,6 +47,22 @@ export default {
     likeMovie(id) {
       this.$store.dispatch('likeMovie', id)
     },
+    getYoutube(id) {
+      axios({
+        method: 'get',
+        url: `https://api.themoviedb.org/3/movie/${id}/videos?language=ko-KR&api_key=${API_KEY}`
+      })
+        .then(res => {
+          // console.log(res.data.results[0].key)
+          this.youtubeId = res.data.results[0].key
+          const youtubesrc = `https://www.youtube.com/embed/${this.youtubeId}?autoplay=1`
+          this.youtubesrc = youtubesrc
+        })
+        .catch(err => console.log(err))
+    }
+  },
+  created() {
+    this.getYoutube(this.detail_movie.id)
   }
 }
 </script>
