ea59cff7db4c85d70d4a6f6d08a54cd742ef8548
권소정 <flfk33@naver.com>
2023-05-23 17:28:43 +0900

    Feat: Add fontawesome

--- final-pjt-front/src/main.js
+++ final-pjt-front/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
+
 
 Vue.config.productionTip = false
 
@@ -10,3 +14,6 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+library.add(faChevronCircleLeft, faChevronCircleRight)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
