989ad9b63b4876f718f3e12fcf469e0c34470fb1
권소정 <flfk33@naver.com>
2023-05-23 16:03:13 +0900

    Feat: Add recommend movie function

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="home">
+    <h1>지금 핫한 영화!</h1>
+    <div v-for="(movie, index) in recommendMovie" :key="index">
+      {{ movie }}
+      <br><br>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import _ from 'lodash'
 
 export default {
   name: 'HomeView',
   components: {
   },
+  data() {
+    return {
+      recommendMovie: [],
+    }
+  },
+  computed: {
+    popularMovies() {
+      return this.$store.state.popularMovies
+    }
+  },
+  methods: {
+    getRecommendMovie() {
+      this.recommendMovie.push(_.sample(this.popularMovies, 1))
+      this.recommendMovie.push(_.sample(this.popularMovies, 1))
+      this.recommendMovie.push(_.sample(this.popularMovies, 1))
+    }
+  },
+  created() {
+    this.getRecommendMovie()
+  }
 }
 </script>
