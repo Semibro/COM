59f75a4dff04eac55f20c475a164fe5af96fb9ab
김준형 (JunHyung Kim) <wnsgud6232@naver.com>
2023-09-10 23:56:59 +0900

    Fix: 개인정보 삭제

--- final-pjt-back/secret_key.py
+++ final-pjt-back/secret_key.py
@@ -1,1 +1,1 @@
-access_key = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODc2NzY1OTc1MTAwOTAxNzdmNTIxN2VhNWY0ZDI4MCIsInN1YiI6IjYzZDMxYjU4NjZhZTRkMDBjMWNmOWFjYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dvRNioXzoWWObV1dbaZ_sJlSGd3MeyQkDG6Gybxtdr0'
+access_key = ''
